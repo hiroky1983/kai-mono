38140f8722a8d7710d6a637f9ec05e4e3604847c
hiroky1983 <75672270+hiroky1983@users.noreply.github.com>
2022-01-11 18:45:23 +0900

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+
+      </main>
+    </div>
+  );
+}
