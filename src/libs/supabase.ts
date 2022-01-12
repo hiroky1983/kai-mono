41a8985861a2ff2ffc6d89cea01fa4ce574b0a61
yamadahiroki <hirockysan1983@gmail.com>
2022-01-12 20:00:51 +0900

    ✨ コメント削除

--- src/libs/supabase.ts
+++ src/libs/supabase.ts
@@ -1,10 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-// 環境変数のロード
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
-// 環境変数が設定されていなかったらエラーを投げる
 if (!SUPABASE_URL) {
   throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
 }
