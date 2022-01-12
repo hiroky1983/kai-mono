b8ddf272bf917fd2a7296aaf939f896bbb952298
yamadahiroki <hirockysan1983@gmail.com>
2022-01-12 20:01:18 +0900

    ✨ _appの型を定義

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
+import { AppProps } from 'next/dist/shared/lib/router/router'
 import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
