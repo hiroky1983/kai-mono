e898b6ae0f1aae76fef64deb370248751c00c8f7
yamadahiroki <hirockysan1983@gmail.com>
2022-01-13 13:35:49 +0900

    ✨ Layout変更

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode, VFC } from "react";
+import { Footer } from "./Footer";
 import { Header } from "./Header";
 
 type Props = {
@@ -8,11 +9,12 @@ type Props = {
 export const Layout: VFC<Props> = (props) => {
   return (
     <div className="bg-gray-300">
-      <div className="container mx-auto grid grid-rows-[auto,1fr,auto] min-h-screen">
+      <div className="container mx-auto grid grid-rows-[auto,1fr] min-h-screen">
         <Header />
         <main className="px-4 text-gray-600 bg-gray-100">
           <div>{props.children}</div>
         </main>
+        <Footer />
       </div>
     </div>
   );
