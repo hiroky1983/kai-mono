9c92ed4257f9ec550335b2a820d990d23e57e48f
yamadahiroki <hirockysan1983@gmail.com>
2022-01-14 08:07:40 +0900

    ✨ 買い物リストをコンポーネント化

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,10 +2,11 @@ import React, { VFC } from "react";
 
 type Props = {
   items: string[];
+  onClickShoppedItems: (i: number) => void;
 };
 
 export const List: VFC<Props> = (props) => {
-  const { items } = props;
+  const { items, onClickShoppedItems } = props;
   return (
     <div>
       <ul>
@@ -17,13 +18,10 @@ export const List: VFC<Props> = (props) => {
             >
               <li className="flex-grow">{item}</li>
               <button
-                onClick={() => onClickAddItems(i)}
+                onClick={() => onClickShoppedItems(i)}
                 className="inline-block px-3 py-1 bg-green-400 text-white rounded-lg"
               >
-                OK
-              </button>
-              <button className="inline-block px-3 py-1 bg-green-400 text-white rounded-lg">
-                要らない
+                買った
               </button>
             </div>
           );
