915bd81a55ad1f6896db0882f0f933b5afbacd74
yamadahiroki <hirockysan1983@gmail.com>
2022-01-24 19:12:39 +0900

    ✨ ドキュメントの書き換え

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,16 +39,16 @@ class MyDocument extends Document {
           {/* safari */}
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="#000" />
-          <meta name="apple-mobile-web-app-title" content="myapp" />
+          <meta name="apple-mobile-web-app-title" content="kai-mono" />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
             href="/apple-touch-icon-180x180.png"
           />
           {/* 一般 */}
-          <meta name="application-name" content="myapp" />
+          <meta name="application-name" content="kai-mono" />
           <meta name="theme-color" content="#000" />
-          <meta name="description" content="this is myapp" />
+          <meta name="description" content="買い物リスト共有アプリです" />
           <link rel="icon" sizes="192x192" href="/icon-192x192.png" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
