4e4add738c6018488ccaa089858985ef431f18fb
hiroky <hirockysan1983@gmail.com>
2022-01-25 16:16:48 +0900

    過去リスト作成

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,20 @@ import React, { VFC } from "react";
 import { Button, Divider, Dropdown, IconClipboard, IconLogOut, IconMail, IconUsers, Typography } from "@supabase/ui";
 
 import { supabase } from "../libs/supabase";
+import { useRouter } from "next/dist/client/router";
 
 const THIS_YEAR = new Date().getFullYear();
 
 export const Footer: VFC = () => {
+  const router = useRouter();
   return (
     <footer className="flex text-gray-600 bg-gray-200 dark:text-white dark:bg-gray-700 items-center gap-4">
       <Dropdown
         overlay={[
-          <Dropdown.Item icon={<IconClipboard />}>
+          <Dropdown.Item
+            onClick={() => router.push("/history")}
+            icon={<IconClipboard />}
+          >
             <Typography.Text>Copy</Typography.Text>
           </Dropdown.Item>,
           <Dropdown.Item icon={<IconMail />}>
@@ -31,7 +36,6 @@ export const Footer: VFC = () => {
       <small lang="en">
         &copy; {THIS_YEAR} hirocky1983 All Rights Reserved.
       </small>
-
     </footer>
   );
 };
---
+++ src/pages/history.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from "next";
+import { Layout } from "../components/Layout";
+
+const History: NextPage = () => {
+
+
+
+    return (
+        <Layout>
+            <div className="mt-4">
+                <h2 className="text-xl underline">過去のリスト</h2>
+                <ul>
+
+                </ul>
+            </div>
+        </Layout>
+    );
+};
+
+export default History;
