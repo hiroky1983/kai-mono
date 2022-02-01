a5131f1028e05e7f1b5fd379bb5b1c886fe0209b
hiroky <hirockysan1983@gmail.com>
2022-02-01 14:09:25 +0900

    不要な関数削除

--- src/libs/supabase.ts
+++ src/libs/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
-const sampleEmail = process.env.NEXT_PUBLIC_SAMPLE_EMAIL;
-const samplePassword = process.env.NEXT_PUBLIC_SAMPLE_PASSWORD;
+export const sampleEmail = process.env.NEXT_PUBLIC_SAMPLE_EMAIL;
+export const samplePassword = process.env.NEXT_PUBLIC_SAMPLE_PASSWORD;
 
 if (!SUPABASE_URL) {
   throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
@@ -13,13 +13,4 @@ if (!SUPABASE_KEY) {
 }
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-export const handleLogin = async () => {
-  try {
-    await supabase.auth.signIn({
-      email: sampleEmail,
-      password: samplePassword,
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
+
