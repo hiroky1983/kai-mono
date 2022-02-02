bb91a35393663d1f287c385676e458b2213f73d7
yamadahiroki <hirockysan1983@gmail.com>
2022-02-02 21:11:33 +0900

    ファビコン等の置き換え

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,43 +2,71 @@ import { NextPage } from "next";
 import Image from "next/image";
 import { Layout } from "../components/Layout";
 
-
 const About: NextPage = () => {
-    return (
-        <Layout>
-            <div className="relative">
-                <Image src="/gettyimages-542930780-612x612.jpeg" layout="responsive" width="300px" height="200px" />
-                <p className="absolute top-6 left-6 text-white p-3 bg-gray-800 bg-opacity-30 font-extrabold">買い物をもっと効率的に</p>
+  return (
+    <Layout>
+      <div className="relative">
+        <Image
+          src="/gettyimages-542930780-612x612.jpeg"
+          layout="responsive"
+          width="300px"
+          height="200px"
+        />
+        <p className="absolute top-6 left-6 text-white p-3 bg-gray-800 bg-opacity-30 font-extrabold">
+          買い物をもっと効率的に
+        </p>
+      </div>
+      <div className="mx-4">
+        <h2 className="text-center py-6 font-bold text-2xl">
+          このアプリでできること
+        </h2>
+        <div className="flex justify-center my-4 text-center">
+          <div className="grid gap-4 md:flex">
+            <div className="w-80 shadow-lg p-6 rounded-lg dark:bg-gray-500">
+              <div className="flex justify-center">
+                <Image src="/Shopping-bro .png" width="180px" height="240px" />
+              </div>
+              <h3 className="py-2 font-bold">パートナーとのリストの共有</h3>
+              <p>買うもののリストを相手と共有できます</p>
             </div>
-            <h2 className="text-center py-6 font-bold text-2xl">このアプリでできること</h2>
-            <div className="flex justify-center my-4 text-center">
-                <div className="grid gap-4 md:flex">
-                    <div className="w-80 shadow-lg p-6 rounded-lg dark:bg-gray-500">
-                        <div className="flex justify-center">
-                            <Image src="/Shopping-bro .png" width="120px" height="240px" />
-                        </div>
-                        <h3 className="py-2 font-bold">パートナーとのリストの共有</h3>
-                        <p>買うもののリストを相手と共有できます</p>
-                    </div>
-                    <div className="w-80 shadow-lg p-6 rounded-lg dark:bg-gray-500">
-                        <div className="flex justify-center">
-                            <Image src="/Add tasks-rafiki .png" width="120px" height="240px" />
-                        </div>
-                        <h3 className="py-2 font-bold">購入後の完了確認</h3>
-                        <p>パートナーに買うものを事前に確認してもらい買うものの重複を防げ余分なコストを削減できます</p>
-                    </div>
-                    <div className="w-80 shadow-lg p-6 rounded-lg dark:bg-gray-500">
-                        <div className="flex justify-center">
-                            <Image src="/Thinking face-rafiki .png" width="120px" height="240px" />
-                        </div>
-                        <h3 className="py-2 font-bold">買い物時スマホからリストを確認</h3>
-                        <p>買いもののときの買い忘れを防ぐことができます</p>
-                    </div>
-                </div>
+            <div className="w-80 shadow-lg p-6 rounded-lg dark:bg-gray-500">
+              <div className="flex justify-center">
+                <Image
+                  src="/Add tasks-rafiki .png"
+                  width="180px"
+                  height="240px"
+                />
+              </div>
+              <h3 className="py-2 font-bold">購入後の完了確認</h3>
+              <p>
+                パートナーに買うものを事前に確認してもらい買うものの重複を防げ余分なコストを削減できます
+              </p>
             </div>
-            <h2 className="text-center py-6 font-bold text-2xl">PWA対応でスマホアプリのように使える</h2>
-        </Layout>
-    )
-}
+            <div className="w-80 shadow-lg p-6 rounded-lg dark:bg-gray-500">
+              <div className="flex justify-center">
+                <Image
+                  src="/Thinking face-rafiki .png"
+                  width="180px"
+                  height="240px"
+                />
+              </div>
+              <h3 className="py-2 font-bold">買い物時スマホからリストを確認</h3>
+              <p>買いもののときの買い忘れを防ぐことができます</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-center py-6 font-bold text-2xl">
+            PWA対応でスマホアプリのように使える
+          </h2>
+          <Image src="/pwadisc1.png" width="200px" height="200px" />
+          <Image src="/pwadisc2.png" width="200px" height="200px" />
+          <Image src="/pwadisc3.png" width="200px" height="200px" />
+          <Image src="/pwadisc4.png" width="200px" height="200px" />
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default About;
