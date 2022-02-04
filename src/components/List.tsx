4109e2ff571afcb21bd93df9dd851b72b4303e5f
hiroky <hirockysan1983@gmail.com>
2022-02-04 13:45:55 +0900

    ファイルの型定義を記述

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,5 @@
-import React, { VFC } from "react";
+import { IconCamera } from "@supabase/ui";
+import React, { ChangeEvent, useState, VFC } from "react";
 import { ItemsData } from "../pages";
 
 type Props = {
@@ -8,6 +9,13 @@ type Props = {
 
 export const List: VFC<Props> = (props) => {
   const { item, onClickShoppedItems } = props;
+  const [file, setFile] = useState<File | undefined>(null);
+  const handleChangeFile = (e: ChangeEvent<HTMLInputElement>) => {
+    setFile(e.target.files[0]);
+  };
+
+  console.log(file);
+
   return (
     <ul>
       <div
@@ -21,6 +29,10 @@ export const List: VFC<Props> = (props) => {
         >
           買った
         </button>
+        <label className="inline-block px-3 lg:px-5 py-1 lg:py-2 bg-white text-primary rounded-lg">
+          <IconCamera />
+          <input type="file" className="hidden z-10" onChange={handleChangeFile} />
+        </label>
       </div>
     </ul>
   );
